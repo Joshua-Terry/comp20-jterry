c0cdb84b12fd15da9c27eab86350253029ea59f5
Joshua-Terry <Joshua.Terry@tufts.edu>
2016-03-09 15:08:45 -0500

    pushing final tweaks

--- messages/lab.js
+++ messages/lab.js
@@ -8,16 +8,16 @@
 		request.onreadystatechange = function() {
 			// Step 3: Data is ready --there is a response!
 			if (request.readyState == 4) {
-				message = "";
+				// message = "";
 				raw = request.responseText;
 				messageData = JSON.parse(raw);
-				elem = document.getElementById("list");
+				elem = document.getElementById("messages");
 			 for (i = 0; i < messageData.length; i++) {
 				
-				message += "<p>" + messageData[i]["username"] + " said " + messageData[i]["content"] + "</p>";
+				elem.innerHTML += "<p>" + messageData[i]["username"] + " said " + messageData[i]["content"] + "</p>";
 				//result += "<p>Next Red Line train to " + theScheduleData["TripList"]["Trips"][i]["Predictions"][0]["Stop"] + ", " + theScheduleData["TripList"]["Trips"][i]["Destination"] + " bound, will come in " + theScheduleData["TripList"]["Trips"][i]["Predictions"][0]["Seconds"] + " seconds</p>";
 			}
-			elem.innerHTML = result;
+			// elem.innerHTML = message;
 			}
 		};
 		//Step 3: Fire off the request
